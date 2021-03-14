d1d333409bcdd9303d11cf34d93354c40458a256
Sven Kronshage <sven@kronsi.com>
2021-03-14 09:28:45 +0100

    try

--- src/Layout/Right.js
+++ src/Layout/Right.js
@@ -57,7 +57,32 @@ class Right extends React.Component {
             textToCopy += zipcode + "\t" + showLatLngList[i].lat + "\t" + showLatLngList[i].lng + "\n";
         }
         console.log("textToCopy", textToCopy);
-        navigator.clipboard.writeText(textToCopy);
+        //navigator.clipboard.writeText(textToCopy);
+        this.copyToClipboard(textToCopy);
+    }
+
+    copyToClipboard(textToCopy) {
+        // navigator clipboard api needs a secure context (https)
+        if (navigator.clipboard && window.isSecureContext) {
+            // navigator clipboard api method'
+            return navigator.clipboard.writeText(textToCopy);
+        } else {
+            // text area method
+            let textArea = document.createElement("textarea");
+            textArea.value = textToCopy;
+            // make the textarea out of viewport
+            textArea.style.position = "fixed";
+            textArea.style.left = "-999999px";
+            textArea.style.top = "-999999px";
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+            return new Promise((res, rej) => {
+                // here the magic happens
+                document.execCommand('copy') ? res() : rej();
+                textArea.remove();
+            });
+        }
     }
 
     onClear = (e) => {
